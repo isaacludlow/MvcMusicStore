afa092a7a37e741eebf7685cc74301153cdbb7a4
Isaac Ludlow <isaac@theludlows.org>
2020-04-05 17:03:04 -0400

    Add project files.

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,39 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDb = new MusicStoreEntities();
+
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var genres = storeDb.Genres.ToList();
+
+            return View(genres);
+        }
+
+        // GET: /Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = storeDb.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+
+            return View(genreModel);
+        }
+
+        // GET: /Store/Details
+        public ActionResult Details(int id)
+        {
+            var album = storeDb.Albums.Find(id);
+
+            return View(album);
+        }
+    }
+}
